137c64a9112a43b9303077dca6b540dbe66c2288
matanfrank <mtnfrnk@gmail.com>
2022-04-11 04:52:00 +0300

    complete profiler

--- src/app/components/text-to-speech/text-to-speech.component.ts
+++ src/app/components/text-to-speech/text-to-speech.component.ts
@@ -1,18 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { SpeechSynthesisService } from 'src/app/service/speech-synthesis.service';
+import { Component } from '@angular/core';
+import { SpeechSynthesisService } from '../../service/speech-synthesis.service';
 
 @Component({
   selector: 'app-text-to-speech',
   templateUrl: './text-to-speech.component.html',
   styleUrls: ['./text-to-speech.component.sass']
 })
-export class TextToSpeechComponent implements OnInit {
+export class TextToSpeechComponent {
 
   public textValue: any;
   constructor(public speechSynthesisService: SpeechSynthesisService) { }
-
-  ngOnInit(): void {
-  }
 
   speak() {
     this.textValue = "hello world";
